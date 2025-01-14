ab8c144932f3f5ad18b8c6c82bdf89c018dcba81
005abhi <abhinandan111003@gmai.com>
2025-01-14 21:08:16 +0530

    final

---
+++ next.config.ts
@@ -0,0 +1,20 @@
+import type { NextConfig } from "next";
+import type { Configuration } from "webpack";
+
+const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  webpack(config: Configuration) {
+    config.module?.rules?.push({
+      test: /\.(mp4|webm)$/,
+      type: "asset/resource",
+      generator: {
+        filename: "static/media/[name].[hash][ext]",
+      },
+    });
+    return config;
+  },
+};
+
+export default nextConfig;
